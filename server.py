e92b2657433bdbfa799ad8037325c9824e855a15
Arata <newbluelight1031@gmail.com>
2023-02-10 18:03:22 +0900

    routing

--- server.py
+++ server.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def home_page():
     return render_template('index.html')
+
+@app.route("/<page_name>")
+def show_html(page_name):
+    page_html = page_name + ".html"
+    return render_template(page_html)
